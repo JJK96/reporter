4116137602ed25e1a7adcd6eb46f55bd6d8f3571
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-06-16 15:15:00 +0200

    Improvements to Tools package

--- necessary_files/_packages/tools/tools.lua
+++ necessary_files/_packages/tools/tools.lua
@@ -3,19 +3,29 @@ module(...,package.seeall)
 local tools = {}
 
 function toolslist()
+    local output = ""
     for name, tool in pairs(tools) do
         if tool.references > 0 then
             item = "\\item[" .. name .. "] " .. tool.description 
             if tool.link then
                 item = item .. "\\footnote{\\url{" .. tool.link .. "}}"
             end
-            tex.print(item)
-            tex.print("\\toolslabeltext{" .. name .. "}{" .. name .. "}")
+            output = output .. item .. "\\toolslabeltext{" .. name .. "}{" .. name .. "}"
         end
+    end
+    if not (output == "") then
+        tex.print("\\toolslistheader")
+        tex.print("\\begin{description}")
+        tex.print(output)
+        tex.print("\\end{description}")
     end
 end
 
 function reftool(name)
+    if tools[name] == nil then
+        tex.print("\\PackageError{tools}{The tool \"" .. name .. "\" is not defined}{The tool should be defined using \\declaretool}")
+        return
+    end
     tools[name].references = tools[name].references+ 1
 end
 
