e831136ec45517f98d0d9cb8a130c6f0ecb252dd
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2023-06-28 12:23:01 +0200

    Allow getting template easily when importing

--- reporter/__init__.py
+++ reporter/__init__.py
@@ -2,8 +2,13 @@ from .reporter import Template
 from .config import config
 
 
-def main():
+def get_template():
     template = Template(config.get('template'))
+    return template
+
+
+def main():
+    template = get_template()
     template.commandline.parse_args()
 
 if __name__ == "__main__":
